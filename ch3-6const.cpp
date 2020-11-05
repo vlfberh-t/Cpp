1b835a8f79e6b88b910c3aa4af5c605e25c3df48
vlfberh-t <59538794+vlfberh-t@users.noreply.github.com>
2020-11-05 12:54:57 -0500

    Create ch3-6const.cpp

---
+++ ch3-6const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    const double pi = 22.0 / 7;
+    cout << "The value of constant pi is: " << pi << endl;
+
+    //Uncomment L11 for compiler failure
+    //pi = 345;
+
+    return 0;
+}
